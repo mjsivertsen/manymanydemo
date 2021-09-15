69eb4ecb5bffe45ea084d8df2a3c6356bb989fd2
Michaela Sivertsen <mjsivertsen@msn.com>
2021-09-15 12:26:57 -0600

    lots of setup done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,28 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
-Thing.create(name:Faker::Company.name )
-Thing.create(name:Faker::Company.name )
+
+Grade.destroy_all
+Skill.destroy_all
+User.destroy_all
+
+a = Skill.create(name: 'ruby', description: 'syntax sugar')
+b = Skill.create(name: 'rails', description: 'stay on the rails... convention')
+c = Skill.create(name: 'react', description: 'make writing UI nice!')
+d = Skill.create(name: 'Homework 1', description: 'CRUD ACTIONS')
+e = Skill.create(name: 'Homework 2', description: 'Cassino')
+
+skills = [a,b,c,d,e]
+
+5.times do |i|
+  user = User.create(name: Faker.name)
+    5.times do |i|
+      Grade.create(score: rand(100), skill_id:skills[i].id, user_id: user.id)
+    end
+end
+
+puts "Skill Size: #{Skill.all.length}"
+puts "User Size: #{User.all.length}"
+puts "Grade Size: #{Grade.all.length}"
+puts "First User Skills: #{User.first.skills}"
+puts "First User Grades: #{User.first.grades}"
